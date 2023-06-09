ae2ddea44a85d6f872f6bb276d723f21049fe568
Abhisek Bajoria <abhisek.bajoria@publicissapient.com>
2023-06-09 08:33:47 +0530

    creating jenkins pipeline

--- src/main/java/com/online/travel/register/service/HotelService.java
+++ src/main/java/com/online/travel/register/service/HotelService.java
@@ -3,7 +3,6 @@ package com.online.travel.register.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.online.travel.register.entity.Hotel;
 import com.online.travel.register.repository.HotelRepository;
-import com.online.travel.register.util.Producer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,12 +12,12 @@ public class HotelService {
     @Autowired
     private HotelRepository hotelRepository;
 
-    @Autowired
-    private Producer producer;
+//    @Autowired
+//    private Producer producer;
 
     public Hotel saveHotel(Hotel hotel) throws JsonProcessingException {
         Hotel savedHotel = hotelRepository.save(hotel);
-        producer.sendMessage(savedHotel);
+        //producer.sendMessage(savedHotel);
         return savedHotel;
     }
 }
